47f73a6d36cb991471fb8ea40a06cd9735fd3965
Si-ghub <63141977+Si-ghub@users.noreply.github.com>
2021-05-13 19:43:52 +0300

    Text

--- js/home.js
+++ js/home.js
@@ -1,10 +1,15 @@
 // IMPORT
+
+// SKAICIAI
 import { dalyba } from './components/algebra/dalyba.js';
 import { kvadratas } from './components/algebra/kvadratas.js';
 import { sandauga } from './components/algebra/sandauga.js';
 import { skirtumas } from './components/algebra/skirtumas.js';
 import { suma } from './components/algebra/suma.js';
 import { vidurkis } from './components/algebra/vidurkis.js';
+
+// TEKSTAI
+import { vientisasTekstas } from './components/tekstas/vientisasTekstas.js';
 
 // EXECUTION
 
@@ -25,3 +30,18 @@ console.log(e);
 
 const f = vidurkis([10, 8]);
 console.log(f);
+
+const s1 = vientisasTekstas(['labas', 'rytas']);
+console.log(s1, '->', 'lbsrts');
+
+const s2 = vientisasTekstas(['mano', 'namas', 'yra', 'geltonas']);
+console.log(s2, '->', 'maonmsyragts');
+
+const s3 = vientisasTekstas(['as', 'i', 'ten']);
+console.log(s3, '->', 'aasiiiten');
+
+const s4 = vientisasTekstas(['']);
+console.log(s4, '->', '');
+
+const s5 = vientisasTekstas(['x']);
+console.log(s5, '->', 'xxx');
